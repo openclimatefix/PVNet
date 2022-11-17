40dea9930b520337cb9657b19d00e40ef6945ec4
BumpVersion Action <bumpversion@github-actions>
2022-11-17 14:04:27 +0000

    Bump version: 0.0.5 → 0.0.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = (this_directory / "requirements.txt").read_text().splitlines(
 
 setup(
     name="PVNet",
-    version="0.0.5",
+    version="0.0.6",
     license="MIT",
     description="PVNet",
     author="Peter Dudfield",
