9aac30369d8daa0d5879b8ec3331f34d2c2f523c
peterdudfield <peter.dudfield@hotmail.com>
2022-12-20 11:47:04 +0000

    add pipline to benchmack

--- scripts/benchmark_gcp_dataset.py
+++ scripts/benchmark_gcp_dataset.py
@@ -2,6 +2,7 @@ from ocf_datapipes.config.load import load_yaml_configuration
 from ocf_datapipes.training.gsp_pv_satellite_nwp import gsp_pv_nwp_satellite_data_pipeline
 from ocf_datapipes.training.pv_satellite_nwp import pv_nwp_satellite_data_pipeline
 from ocf_datapipes.training.simple_pv import simple_pv_datapipe
+from ocf_datapipes.training.nwp_pv import nwp_pv_datapipe
 
 import time
 
@@ -23,8 +24,8 @@ config.process.batch_size = 2
 
 
 # just load pv data
-for pipeline_object in [simple_pv_datapipe, pv_nwp_satellite_data_pipeline, gsp_pv_nwp_satellite_data_pipeline]:
-    pipeline = pipeline_object(configuration=file).set_length(5)
+for pipeline_object in [simple_pv_datapipe,nwp_pv_datapipe, pv_nwp_satellite_data_pipeline, gsp_pv_nwp_satellite_data_pipeline]:
+    pipeline = pipeline_object(config).set_length(5)
     dl = DataLoader2(dataset=pipeline, batch_size=None, num_workers=0)
     dl_iter = iter(pipeline)
 
