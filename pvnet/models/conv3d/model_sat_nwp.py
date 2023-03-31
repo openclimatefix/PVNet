2b17901b1b8f70c78533285eba8769ce19347020
James Fulton <djamesfulton@yahoo.co.uk>
2023-03-31 12:21:25 +0000

    make sat optional

--- pvnet/models/conv3d/model_sat_nwp.py
+++ pvnet/models/conv3d/model_sat_nwp.py
@@ -36,6 +36,7 @@ class Model(BaseModel):
         output_network: Pytorch Module class used to combine the 1D features to produce the 
             forecast.
         output_network_kwrgs: Dictionary of optional kwargs for the `output_network` module.
+        include_sat: Include satellite data.
         include_nwp: Include NWP data.
         include_gsp_yield_history: Include GSP yield data.
         include_sun: Include sun azimuth and altitude data.
@@ -65,8 +66,9 @@ class Model(BaseModel):
         encoder_kwargs: dict = dict(),
         
         output_network: AbstractTabularNetwork = pvnet.models.conv3d.dense_networks.DefaultFCNet,
-        output_network_kwrgs: dict = dict(),
+        output_network_kwargs: dict = dict(),
         
+        include_sat: bool = True,
         include_nwp: bool = True,
         include_gsp_yield_history: bool = True,
         include_sun: bool = True,
@@ -87,25 +89,26 @@ class Model(BaseModel):
 
 
         self.include_gsp_yield_history = include_gsp_yield_history
+        self.include_sat = include_sat
         self.include_nwp = include_nwp
         self.include_sun = include_sun
         self.embedding_dim = embedding_dim
         
         super().__init__(history_minutes, forecast_minutes, optimizer)
         
-        # TODO: remove this hardcoding
-        # We limit the history to have a delay of 15 mins in satellite data
-        if sat_history_minutes is None: sat_history_minutes = history_minutes
-        
-        self.sat_sequence_len = sat_history_minutes//5 + 1 - 3
-        
-        self.sat_encoder = image_encoder(
-            sequence_length=self.sat_sequence_len,
-            image_size_pixels=sat_image_size_pixels,
-            in_channels=number_sat_channels,
-            out_features=encoder_out_features,
-            **encoder_kwargs,
-        )
+        if include_sat:
+            # TODO: remove this hardcoding
+            # We limit the history to have a delay of 15 mins in satellite data
+            if sat_history_minutes is None: sat_history_minutes = history_minutes
+            self.sat_sequence_len = sat_history_minutes//5 + 1 - 3
+
+            self.sat_encoder = image_encoder(
+                sequence_length=self.sat_sequence_len,
+                image_size_pixels=sat_image_size_pixels,
+                in_channels=number_sat_channels,
+                out_features=encoder_out_features,
+                **encoder_kwargs,
+            )
 
         if include_nwp:
             if nwp_history_minutes is None: nwp_history_minutes = history_minutes
@@ -133,7 +136,9 @@ class Model(BaseModel):
                 out_features=16,
             )
 
-        fc_in_features = encoder_out_features
+        fc_in_features = 0
+        if include_sat:
+            fc_in_features += encoder_out_features
         if include_nwp:
             fc_in_features += encoder_out_features
         if include_gsp_yield_history:
@@ -142,24 +147,27 @@ class Model(BaseModel):
             fc_in_features += embedding_dim
         if include_sun:
             fc_in_features += 16
-
+    
         self.output_network = output_network(
             in_features=fc_in_features,
             out_features=self.forecast_len,
+            **output_network_kwargs,
         )
 
 
     def forward(self, x):
+        
+        out = []
         # ******************* Satellite imagery *************************
-        # Shape: batch_size, seq_length, channel, height, width
-        sat_data = x[BatchKey.satellite_actual]
-        batch_size, seq_len, n_chans, height, width = sat_data.shape
-        # switch time and channels
-        sat_data = torch.swapaxes(sat_data, 1, 2).float()
+        if self.include_sat:
+            # Shape: batch_size, seq_length, channel, height, width
+            sat_data = x[BatchKey.satellite_actual]
+            # switch time and channels
+            sat_data = torch.swapaxes(sat_data, 1, 2).float()
 
-        sat_data = sat_data[:, :, : self.sat_sequence_len]
+            sat_data = sat_data[:, :, : self.sat_sequence_len]
 
-        out = self.sat_encoder(sat_data)
+            out += [self.sat_encoder(sat_data)]
         
         # *********************** GSP Data ************************************
         # add gsp yield history
@@ -168,7 +176,7 @@ class Model(BaseModel):
 
             gsp_history = gsp_history.reshape(gsp_history.shape[0], -1)
             
-            out = torch.cat((out, gsp_history), dim=1)
+            out += [gsp_history]
 
         # *********************** NWP Data ************************************
         if self.include_nwp:
@@ -179,16 +187,14 @@ class Model(BaseModel):
 
             nwp_out = self.nwp_encoder(nwp_data)
 
-            # join with other FC layer
-            out = torch.cat((out, nwp_out), dim=1)
+            out += [nwp_out]
 
         # ********************** Embedding of GSP ID ********************
         if self.embedding_dim:
-            id = x[BatchKey.gsp_id][:, 0]
-            id = id.type(torch.IntTensor)
-            id = id.to(out.device)
+            id = x[BatchKey.gsp_id][:, 0].int()
+            #id = id.to(out.device)
             id_embedding = self.embed(id)
-            out = torch.cat((out, id_embedding), dim=1)
+            out += [id_embedding]
 
         if self.include_sun:
             sun = torch.cat(
@@ -196,25 +202,26 @@ class Model(BaseModel):
                 dim=1
             ).float()
             sun_out = self.sun_fc1(sun)
-            out = torch.cat((out, sun_out), dim=1)
-                    
+            out += [sun_out]
+        
+        out = torch.cat(out, dim=1)    
         out = self.output_network(out)
 
         return out
     
     
 if __name__=="__main__":
-    
+    from torch.optim import SGD
     history = 60
     forecast = 30
 
-    sun_in = torch.zeros((3, (history+forecast)//30+1))
-    gsp_id = torch.zeros((3, 1))
+    sun_in = torch.rand((3, (history+forecast)//30+1))
+    gsp_id = torch.rand((3, 1))
     # Shape: batch_size, seq_length, channel, height, width
-    sat_data = torch.zeros((3, history//5 +1 - 3, 11, 24, 24))
+    sat_data = torch.rand((3, history//5 +1 - 3, 11, 24, 24))
     # shape: batch_size, seq_len, n_chans, height, width
-    nwp_data = torch.zeros((3, (history+forecast)//60+1, 2, 12, 12))
-    gsp = torch.zeros((3, (history+forecast)//30+1))
+    nwp_data = torch.rand((3, (history+forecast)//60+1, 2, 24, 24))
+    gsp = torch.rand((3, (history+forecast)//30+1))
 
     batch = {
         BatchKey.gsp_solar_azimuth: sun_in,
@@ -228,11 +235,18 @@ if __name__=="__main__":
 
 
     model = Model(
-        image_encoder = pvnet.models.conv3d.encoders.EncoderNaiveEfficientNet,
-        output_network = pvnet.models.conv3d.dense_networks.DefaultFCNet,
+        image_encoder = pvnet.models.conv3d.encoders.DefaultPVNet,
         encoder_kwargs = dict(),
-        output_network_kwrgs = dict(),
+        #image_encoder = pvnet.models.conv3d.encoders.EncoderUNET,
+        #encoder_kwargs = dict(n_downscale=3),
+        
+        output_network = pvnet.models.conv3d.dense_networks.DefaultFCNet,
+        output_network_kwargs = dict(),
+        #output_network = pvnet.models.conv3d.dense_networks.ResFCNet,
+        #output_network_kwargs = dict(),
+        
         include_gsp_yield_history = True,
+        include_sat = True,
         include_nwp = True,
         forecast_minutes = 30,
         history_minutes = 60,
@@ -240,7 +254,7 @@ if __name__=="__main__":
         nwp_forecast_minutes = None,
         nwp_history_minutes = None,
         sat_image_size_pixels = 24,
-        nwp_image_size_pixels = 12,
+        nwp_image_size_pixels = 24,
         number_sat_channels = 11,
         number_nwp_channels = 2,
         encoder_out_features = 64,
@@ -248,5 +262,9 @@ if __name__=="__main__":
         include_sun = True,
     )
     
-    print(model)
+    opt = SGD(model.parameters(), lr=0.001)
+    
+    #print(model)
     print(model(batch))
+    model(batch).sum().backward()
+    
