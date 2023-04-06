b572d0a064f9889fcef156cc826cf46e20600458
James Fulton <djamesfulton@yahoo.co.uk>
2023-04-06 13:19:30 +0000

    new optimizer

--- pvnet/optimizers.py
+++ pvnet/optimizers.py
@@ -34,3 +34,22 @@ class AdamW(AbstractOptimizer):
         
     def __call__(self, model_parameters):
         return torch.optim.AdamW(model_parameters, lr=self.lr, **self.kwargs)
+    
+class AdamWReduceLROnPlateau(AbstractOptimizer):
+    
+    def __init__(self, lr=0.0005, patience=3, factor=0.5, threshold=2e-4):
+        self.lr = lr
+        self.patience = patience
+        self.factor = factor
+        self.threshold = threshold
+        
+    def __call__(self, model_parameters):
+        opt = torch.optim.AdamW(model_parameters, lr=self.lr)
+        sch = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            opt,
+            factor=self.factor,
+            patience=self.patience,
+            threshold=self.threshold,
+        )
+        sch = {"scheduler": sch, "monitor": "MAE/train"}
+        return [opt], [sch]
