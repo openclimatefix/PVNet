8fc128c57da496782ca6d571a907631ed17ab3d0
James Fulton <djamesfulton@yahoo.co.uk>
2023-04-06 13:22:40 +0000

    use dataloader2 and allow loading from pre-saved batches

--- pvnet/data/datamodule.py
+++ pvnet/data/datamodule.py
@@ -1,38 +1,65 @@
 """ Data module for pytorch lightning """
 from typing import Callable, Union
-
-import fsspec.asyn
-from lightning.pytorch import LightningDataModule
-from torch.utils.data import DataLoader, default_collate
-import numpy as np
-import torch
-
-from ocf_datapipes.batch.fake.fake_batch import fake_data_pipeline
-from ocf_datapipes.training.pvnet import pvnet_datapipe
-
-from ocf_datapipes.utils.utils import (
-    stack_np_examples_into_batch,
-    set_fsspec_for_multiprocess,
-)
-
+from glob import glob
 from datetime import datetime
 
+import numpy as np
+import torch
+from torchdata.datapipes.iter import FileLister
+from torchdata.datapipes import functional_datapipe
 from torchdata.dataloader2 import DataLoader2, MultiProcessingReadingService
+from lightning.pytorch import LightningDataModule
+
+from ocf_datapipes.training.pvnet import pvnet_datapipe
+from ocf_datapipes.utils.utils import stack_np_examples_into_batch
+from ocf_datapipes.utils.consts import BatchKey
 
 
-def worker_init_fn(*args, **kwargs):
-    """Configures each dataset worker process.
-    1. Get fsspec ready for multi process
-    """
-    # fix for fsspec when using multprocess
-    set_fsspec_for_multiprocess()
     
 def batch_to_tensor(batch):
     for k in list(batch.keys()):
         if isinstance(batch[k], np.ndarray) and np.issubdtype(batch[k].dtype, np.number):
             batch[k] = torch.as_tensor(batch[k])    
     return batch
+
+def print_yaml(path):
+    print(f"{path} :")
+    with open(path, mode="r") as stream:
+        print("".join(stream.readlines()))
     
+    
+from torchdata.datapipes.iter import IterDataPipe
+
+
+
+def split_batches(batch):
+    n_samples = batch[BatchKey.gsp].shape[0]
+    keys = list(batch.keys())
+    examples = [{} for _ in range(n_samples)]
+    for i in range(n_samples):
+        b = examples[i]
+        for k in keys:
+            if ("idx" in k.name) or ("channel_names" in k.name):
+                b[k] = batch[k]
+            else:
+                b[k] = batch[k][i]
+    return examples
+
+@functional_datapipe("split_batches")
+class BatchSplitter(IterDataPipe):
+
+    def __init__(self, source_datapipe: IterDataPipe):
+        """
+
+        """
+        self.source_datapipe = source_datapipe
+
+    def __iter__(self):
+        """Opens the NWP data"""
+        for batch in self.source_datapipe:
+            for example in split_batches(batch):
+                yield example
+
 
 class DataModule(LightningDataModule):
     """
@@ -49,13 +76,32 @@ class DataModule(LightningDataModule):
         val_period=[None, None],
         test_period=[None, None],
         block_nwp_and_sat=False,
+        use_premade_batches=False,
+        batch_dir=None,
     ):
 
         super().__init__()
         self.configuration = configuration
         self.batch_size = batch_size
         self.block_nwp_and_sat = block_nwp_and_sat
+        self.use_premade_batches = use_premade_batches
+        self.batch_dir = batch_dir
+        
+        if use_premade_batches:
+            print(
+                f"Loading batches from: {batch_dir}\n"
+                "These batches were saved with the following configs:"
+            )
+            print_yaml(f"{batch_dir}/datamodule.yaml")
+            print_yaml(f"{batch_dir}/data_configuration.yaml")
 
+            n_train = len(glob(f"{batch_dir}/train/*.pt"))
+            n_val = len(glob(f"{batch_dir}/val/*.pt"))
+            
+            print(f"There are {n_train} training batches")
+            print(f"There are {n_val} validation batches")
+
+                    
         self.train_period = [
             None if d is None else datetime.strptime(d, "%Y-%m-%d") 
             for d in train_period
@@ -73,21 +119,9 @@ class DataModule(LightningDataModule):
             num_workers=num_workers,
             multiprocessing_context="spawn",
             worker_prefetch_cnt=prefetch_factor,
-            #worker_init_fn=worker_init_fn,
-        )
-        self.dataloader_config = dict(
-            pin_memory=False,
-            num_workers=num_workers,
-            prefetch_factor=prefetch_factor,
-            worker_init_fn=worker_init_fn,
-            persistent_workers=False,
-            # Disable automatic batching because dataset
-            # returns complete batches.
-            batch_size=None,
         )
         
-    def _get_datapipe(self, start_time, end_time, batch_size):
-
+    def _get_datapipe(self, start_time, end_time):
         data_pipeline = pvnet_datapipe(
             self.configuration, 
             start_time=start_time,
@@ -98,32 +132,65 @@ class DataModule(LightningDataModule):
 
         data_pipeline = (
             data_pipeline
-                .batch(batch_size)
+                .batch(self.batch_size)
                 .map(stack_np_examples_into_batch)
                 .map(batch_to_tensor)
         )   
         return data_pipeline
+    
+    def _get_premade_batches_datapipe(self, subdir, shuffle=False):
+        data_pipeline = FileLister(f"{self.batch_dir}/{subdir}", masks="*.pt", recursive=False)
+        if shuffle:
+            data_pipeline = (
+                data_pipeline
+                .shuffle(buffer_size=10_000)
+                .sharding_filter()
+                .map(torch.load)
+                # Split the batches and reshuffle them to be combined into new batches
+                .split_batches()
+                .shuffle(buffer_size=100*self.batch_size)
+            )
+        else:
+            data_pipeline = (
+                data_pipeline
+                .sharding_filter()
+                .map(torch.load)
+                # Split the batches so we can use any batch-size
+                .split_batches()
+            )
+        
+        data_pipeline = (
+            data_pipeline
+            .batch(self.batch_size)
+            .map(stack_np_examples_into_batch)
+            .map(batch_to_tensor)
+        )
+        
+        return data_pipeline
         
     def train_dataloader(self):
-        datapipe = self._get_datapipe(*self.train_period, self.batch_size)
-        #rs = MultiProcessingReadingService(**self.readingservice_config)
-        #return DataLoader2(datapipe, reading_service=rs)
-        kwargs = dict(**self.dataloader_config)
-        return DataLoader(datapipe, **kwargs)
+        if self.use_premade_batches:
+            datapipe = self._get_premade_batches_datapipe("train", shuffle=True)
+        else:
+            datapipe = self._get_datapipe(*self.train_period)
+        rs = MultiProcessingReadingService(**self.readingservice_config)
+        return DataLoader2(datapipe, reading_service=rs)
         
     def val_dataloader(self):
-        datapipe = self._get_datapipe(*self.val_period, self.batch_size)
-        #rs = MultiProcessingReadingService(**self.readingservice_config)
-        #return DataLoader2(datapipe, reading_service=rs)
-        kwargs = dict(**self.dataloader_config)
-        return DataLoader(datapipe, **kwargs)
+        if self.use_premade_batches:
+            datapipe = self._get_premade_batches_datapipe("val")
+        else:
+            datapipe = self._get_datapipe(*self.val_period)
+        rs = MultiProcessingReadingService(**self.readingservice_config)
+        return DataLoader2(datapipe, reading_service=rs)
         
 
     def test_dataloader(self):
-        datapipe = self._get_datapipe(*self.test_period, self.batch_size)
-        #rs = MultiProcessingReadingService(**self.readingservice_config)
-        #return DataLoader2(datapipe, reading_service=rs)
-        kwargs = dict(**self.dataloader_config)
-        return DataLoader(datapipe, **kwargs)
+        if self.use_premade_batches:
+            datapipe = self._get_premade_batches_datapipe("test")
+        else:
+            datapipe = self._get_datapipe(*self.test_period)
+        rs = MultiProcessingReadingService(**self.readingservice_config)
+        return DataLoader2(datapipe, reading_service=rs)
         
 
