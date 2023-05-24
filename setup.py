f2d234badbdf20296f40afe1394a2346da9f1a19
BumpVersion Action <bumpversion@github-actions>
2023-05-24 11:48:00 +0000

    Bump version: 0.0.14 → 0.0.15

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ extras_require = {"all_models": ["pytorch-tabnet", "efficientnet_pytorch"]}
 
 setup(
     name="PVNet",
-    version="0.0.14",
+    version="0.0.15",
     license="MIT",
     description="PVNet",
     author="Peter Dudfield",
