5de42854cda0900fb0fff8b286ba44c12e123812
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2023-06-08 13:01:02 +0100

    More logging (#26)
    * add max prediction for each batch
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add max of each column after normalized
    
    * change debug to info
    
    * add loggin of gsp capacities
    
    * fix
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- pvnet/app.py
+++ pvnet/app.py
@@ -255,7 +255,7 @@ def app(t0=None, apply_adjuster=True, gsp_ids=gsp_ids, write_predictions=True):
             preds = model(device_batch).detach().cpu().numpy()
 
             # Calculate unnormalised elevation and sun-dowm mask
-            logger.info("Remove predictions after sundown")
+            logger.info("Zeroing predictions after sundown")
             elevation = batch[BatchKey.gsp_solar_elevation] * ELEVATION_STD + ELEVATION_MEAN
             # We only need elevation mask for forecasted values, not history
             elevation = elevation[:, -preds.shape[-1] :]
@@ -288,9 +288,9 @@ def app(t0=None, apply_adjuster=True, gsp_ids=gsp_ids, write_predictions=True):
         ),
     )
     # Multiply normalised forecasts by capacities and clip negatives
+    logger.info(f"Converting to absolute MW using {gsp_capacities}")
     df_abs = df_normed.clip(0, None) * gsp_capacities.T
-    logger.debug(f"Maximum predictions: {df_abs.max()}")
-
+    logger.info(f"Maximum predictions: {df_abs.max()}")
     # ---------------------------------------------------------------------------
     # 6. Make national total
     logger.info("Summing to national forecast")
