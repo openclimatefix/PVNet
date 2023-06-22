65663c8ebf2b18b5079819f98f1282efb21dd5f2
James Fulton <djamesfulton@yahoo.co.uk>
2023-06-22 14:23:21 +0000

    remove duplicate block

--- pvnet/models/multimodal/encoders/basic_blocks.py
+++ pvnet/models/multimodal/encoders/basic_blocks.py
@@ -76,47 +76,6 @@ class ResidualConv3dBlock(nn.Module):
         return self.model(x) + x
 
 
-class ResidualConv3dBlock(nn.Module):
-    """Fully-connected deep network based on ResNet architecture.
-
-    Internally, this network uses ELU activations throughout the residual blocks.
-    """
-
-    def __init__(
-        self,
-        in_channels,
-        n_layers: int = 2,
-        dropout_frac: float = 0.0,
-    ):
-        """Fully-connected deep network based on ResNet architecture.
-
-        Args:
-            in_channels: Number of input channels.
-            n_layers: Number of layers in residual pathway.
-            dropout_frac: Probability of an element to be zeroed.
-        """
-        super().__init__()
-
-        layers = []
-        for i in range(n_layers):
-            layers += [
-                nn.ELU(),
-                nn.Conv3d(
-                    in_channels=in_channels,
-                    out_channels=in_channels,
-                    kernel_size=(3, 3, 3),
-                    padding=(1, 1, 1),
-                ),
-                nn.Dropout3d(p=dropout_frac),
-            ]
-
-        self.model = nn.Sequential(*layers)
-
-    def forward(self, x):
-        """Run residual connection"""
-        return self.model(x) + x
-
-
 class ImageSequenceEncoder(nn.Module):
     """Simple network which independently encodes each image in a sequence into 1D features"""
 
