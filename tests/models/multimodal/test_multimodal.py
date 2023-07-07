339c141ab0262a74fa2689092e4babd585a0f23f
James Fulton <djamesfulton@yahoo.co.uk>
2023-07-07 10:45:41 +0000

    update tests

--- tests/models/multimodal/test_multimodal.py
+++ tests/models/multimodal/test_multimodal.py
@@ -9,6 +9,12 @@ def multimodal_model(multimodal_model_kwargs):
     return model
 
 
+@pytest.fixture()
+def multimodal_quantile_model(multimodal_model_kwargs):
+    model = Model(output_quantiles=[0.1, 0.5, 0.9], **multimodal_model_kwargs)
+    return model
+
+
 def test_model_forward(multimodal_model, sample_batch):
     y = multimodal_model(sample_batch)
 
@@ -24,3 +30,19 @@ def test_model_backward(multimodal_model, sample_batch):
 
     # Backwards on sum drives sum to zero
     y.sum().backward()
+    
+def test_quantile_model_forward(multimodal_quantile_model, sample_batch):
+    y_quantiles = multimodal_quantile_model(sample_batch)
+
+    # check output is the correct shape
+    # batch size=2, forecast_len=15, num_quantiles=3
+    assert tuple(y_quantiles.shape) == (2, 16, 3), y.shape
+
+
+def test_quantile_model_backward(multimodal_quantile_model, sample_batch):
+    opt = SGD(multimodal_quantile_model.parameters(), lr=0.001)
+
+    y_quantiles = multimodal_quantile_model(sample_batch)
+
+    # Backwards on sum drives sum to zero
+    y_quantiles.sum().backward()
