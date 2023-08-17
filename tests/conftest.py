22d102006affcc6671c6a793bb7810c149c01299
James Fulton <djamesfulton@yahoo.co.uk>
2023-08-17 13:05:57 +0000

    remove app

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,17 +5,8 @@ import pandas as pd
 import numpy as np
 import xarray as xr
 import torch
-from nowcasting_datamodel.connection import DatabaseConnection
-from nowcasting_datamodel.models.base import Base_Forecast, Base_PV
-from nowcasting_datamodel.read.read import get_location
-from nowcasting_datamodel.fake import make_fake_me_latest
-from nowcasting_datamodel.models import (
-    GSPYield,
-    LocationSQL,
-)
 
 from ocf_datapipes.utils.consts import BatchKey
-from testcontainers.postgres import PostgresContainer
 from datetime import timedelta
 
 import pvnet
@@ -27,62 +18,6 @@ xr.set_options(keep_attrs=True)
 
 def time_before_present(dt: timedelta):
     return pd.Timestamp.now(tz=None) - dt
-
-
-@pytest.fixture(scope="session")
-def engine_url():
-    """Database engine, this includes the table creation."""
-    with PostgresContainer("postgres:14.5") as postgres:
-        url = postgres.get_connection_url()
-        os.environ["DB_URL"] = url
-
-        database_connection = DatabaseConnection(url, echo=True)
-
-        engine = database_connection.engine
-
-        # Would like to do this here but found the data
-        # was not being deleted when using 'db_connection'
-        # database_connection.create_all()
-        # Base_PV.metadata.create_all(engine)
-
-        yield url
-
-        # Base_PV.metadata.drop_all(engine)
-        # Base_Forecast.metadata.drop_all(engine)
-
-        engine.dispose()
-
-
-@pytest.fixture()
-def db_connection(engine_url):
-    database_connection = DatabaseConnection(engine_url, echo=True)
-
-    engine = database_connection.engine
-    # connection = engine.connect()
-    # transaction = connection.begin()
-
-    # There should be a way to only make the tables once
-    # but make sure we remove the data
-    database_connection.create_all()
-    Base_PV.metadata.create_all(engine)
-
-    yield database_connection
-
-    # transaction.rollback()
-    # connection.close()
-
-    Base_PV.metadata.drop_all(engine)
-    Base_Forecast.metadata.drop_all(engine)
-
-
-@pytest.fixture()
-def db_session(db_connection, engine_url):
-    """Return a sqlalchemy session, which tears down everything properly post-test."""
-
-    with db_connection.get_session() as s:
-        s.begin()
-        yield s
-        s.rollback()
 
 
 @pytest.fixture
@@ -149,46 +84,6 @@ def sat_data():
     del ds.attrs["_data_attrs"]
 
     return ds
-
-
-@pytest.fixture()
-def gsp_yields_and_systems(db_session):
-    """Create gsp yields and systems"""
-
-    # GSP data is mostly up to date
-    t0_datetime_utc = time_before_present(timedelta(minutes=0)).floor(timedelta(minutes=30))
-
-    # this pv systems has same coordiantes as the first gsp
-    gsp_yields = []
-    locations = []
-    for i in range(0, 318):
-        location_sql: LocationSQL = get_location(
-            session=db_session,
-            gsp_id=i,
-            installed_capacity_mw=123.0,
-        )
-
-        gsp_yield_sqls = []
-        # From 3 hours ago to 8.5 hours into future
-        for minute in range(-3 * 60, 9 * 60, 30):
-            gsp_yield_sql = GSPYield(
-                datetime_utc=t0_datetime_utc + timedelta(minutes=minute),
-                solar_generation_kw=np.random.randint(low=0, high=1000),
-                capacity_mwp=100,
-            ).to_orm()
-            gsp_yield_sql.location = location_sql
-            gsp_yields.append(gsp_yield_sql)
-            locations.append(location_sql)
-
-    # add to database
-    db_session.add_all(gsp_yields)
-
-    db_session.commit()
-
-    return {
-        "gsp_yields": gsp_yields,
-        "gs_systems": locations,
-    }
 
 
 @pytest.fixture()
@@ -273,10 +168,3 @@ def multimodal_model_kwargs(model_minutes_kwargs):
     )
     kwargs.update(model_minutes_kwargs)
     return kwargs
-
-
-@pytest.fixture()
-def me_latest(db_session):
-    metric_values = make_fake_me_latest(session=db_session, model_name="pvnet_v2")
-    db_session.add_all(metric_values)
-    db_session.commit()
