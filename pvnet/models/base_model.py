d0697747a6f8dc78964568a01f481262e186cd4d
James Fulton <djamesfulton@yahoo.co.uk>
2023-12-22 11:05:04 +0000

    little bugs

--- pvnet/models/base_model.py
+++ pvnet/models/base_model.py
@@ -370,7 +370,10 @@ class BaseModel(pl.LightningModule, PVNetModelHubMixin):
             # Take median value for remaining metric calculations
             y_hat = self._quantiles_to_prediction(y_hat)
 
-        common_metrics_each_step = common_metrics(predictions=y_hat.numpy(), target=y.numpy())
+        common_metrics_each_step = common_metrics(
+            predictions=y_hat.cpu().numpy(), 
+            target=y.cpu().numpy()
+        )
         mse_each_step = common_metrics_each_step["rmse"] ** 2
         mae_each_step = common_metrics_each_step["mae"]
 
