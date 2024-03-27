b255253d26877b3b0babf6b5ca9b7cfefc8d7d09
Markus Kreft <129367085+markus-kreft@users.noreply.github.com>
2024-03-27 13:27:27 +0100

    Use batch transformation functions from ocf_datapipes

--- pvnet/data/utils.py
+++ pvnet/data/utils.py
@@ -1,34 +1,6 @@
 """Utils common between Wind and PV datamodules"""
-import numpy as np
-import torch
 from ocf_datapipes.batch import BatchKey, unstack_np_batch_into_examples
 from torch.utils.data import IterDataPipe, functional_datapipe
-
-
-def copy_batch_to_device(batch, device):
-    """Moves a dict-batch of tensors to new device."""
-    batch_copy = {}
-
-    for k, v in batch.items():
-        if isinstance(v, dict):
-            # Recursion to reach the nested NWP
-            batch_copy[k] = copy_batch_to_device(v, device)
-        elif isinstance(v, torch.Tensor):
-            batch_copy[k] = v.to(device)
-        else:
-            batch_copy[k] = v
-    return batch_copy
-
-
-def batch_to_tensor(batch):
-    """Moves numpy batch to a tensor"""
-    for k, v in batch.items():
-        if isinstance(v, dict):
-            # Recursion to reach the nested NWP
-            batch[k] = batch_to_tensor(v)
-        elif isinstance(v, np.ndarray) and np.issubdtype(v.dtype, np.number):
-            batch[k] = torch.as_tensor(v)
-    return batch
 
 
 @functional_datapipe("split_batches")
