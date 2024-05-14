ca9dd7cff6373ae73a7f50ef7721320a305c638c
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2024-05-14 16:28:15 +0200

    N samples experiments (#188)
    * update experiments
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add 400 experiment
    
    * update plot
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ experiments/india/004_n_training_samples/log-plot.py
@@ -0,0 +1,14 @@
+""" Small script to make MAE vs number of batches plot"""
+
+import pandas as df
+import plotly.graph_objects as go
+
+data = [[100, 7.779], [300, 7.441], [1000, 7.181], [3000, 7.180], [6711, 7.151]]
+df = df.DataFrame(data, columns=["n_samples", "MAE [%]"])
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=df["n_samples"], y=df["MAE [%]"], mode="lines+markers"))
+fig.update_layout(title="MAE % for N samples", xaxis_title="N Samples", yaxis_title="MAE %")
+# change to log log
+fig.update_xaxes(type="log")
+fig.show(renderer="browser")
