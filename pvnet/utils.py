105551cd32f128b614e5cf734387a2f25de07de0
Sukhil Patel <sukhil@Sukhils-MacBook-Pro.local>
2024-05-15 16:26:45 +0100

    Fix id name

--- pvnet/utils.py
+++ pvnet/utils.py
@@ -68,7 +68,8 @@ class GSPLocationLookup:
             id=gsp_id,
         )
 
-class SiteLocationLookup:
+class SiteLocationLookup: 
+    """Query object for site location from site ID"""
 
     def __init__(self, long: xr.DataArray, lat: xr.DataArray):
         """Query object for site location from site ID
@@ -81,17 +82,17 @@ class SiteLocationLookup:
         self.longitude = long
         self.latitude = lat
 
-    def __call__(self, ml_id: int) -> Location:
-        """Returns the locations for the input GSP IDs.
+    def __call__(self, site_id: int) -> Location:
+        """Returns the locations for the input site IDs.
 
         Args:
-            gsp_id: Integer ID of the GSP
+            site_id: Integer ID of the site
         """
         return Location(
             coordinate_system="lon_lat",
-            x=self.longitude.sel(pv_system_id=ml_id).item(),
-            y=self.latitude.sel(pv_system_id=ml_id).item(),
-            id=ml_id,
+            x=self.longitude.sel(pv_system_id=site_id).item(),
+            y=self.latitude.sel(pv_system_id=site_id).item(),
+            id=site_id,
         )
 
 def extras(config: DictConfig) -> None:
