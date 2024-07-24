6256d28453209dc5c71bb9ac60d4d152e95d3c26
peterdudfield <peter.dudfield@hotmail.com>
2024-07-24 15:12:03 +0100

    update

--- tests/models/multimodal/test_from_pretrained.py
+++ tests/models/multimodal/test_from_pretrained.py
@@ -1,4 +1,4 @@
-from pvnet.models.base_model import PVNetBaseModel
+from pvnet.models.base_model import BaseModel
 
 
 def test_from_pretrained():
@@ -6,7 +6,7 @@ def test_from_pretrained():
     model_name = "openclimatefix/pvnet_v2"
     model_version = "4203e12e719efd93da641c43d2e38527648f4915"
 
-    _ = PVNetBaseModel.from_pretrained(
+    _ = BaseModel.from_pretrained(
         model_name,
         revision=model_version,
     )
