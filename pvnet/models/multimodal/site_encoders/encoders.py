a0937ff25e7adba5006ded3af3fd3334995cf797
devojyotimisra <devojyotimisra1@gmail.com>
2025-03-01 13:35:48 +0530

    removed BtachKey and made necessary keys as type string

--- pvnet/models/multimodal/site_encoders/encoders.py
+++ pvnet/models/multimodal/site_encoders/encoders.py
@@ -4,7 +4,6 @@
 
 import einops
 import torch
-from ocf_datapipes.batch import BatchKey
 from torch import nn
 
 from pvnet.models.multimodal.linear_networks.networks import ResFCNet2
@@ -75,13 +74,15 @@ class SimpleLearnedAggregator(AbstractSitesEncoder):
         )
 
     def _calculate_attention(self, x):
-        gsp_ids = x[BatchKey.gsp_id].squeeze().int()
+        gsp_ids = x["BatchKey.gsp_id"].squeeze().int()
         attention = self._attention_network(gsp_ids)
         return attention
 
     def _encode_value(self, x):
         # Shape: [batch size, sequence length, PV site]
-        pv_site_seqs = x[BatchKey.pv].float()
+        for key in x.keys():
+            print(key)
+        pv_site_seqs = x["BatchKey.pv"].float()
         batch_size = pv_site_seqs.shape[0]
 
         pv_site_seqs = pv_site_seqs.swapaxes(1, 2).flatten(0, 1)
