69ba024237fa8be140a886c59dd1d4511f772227
James Fulton <41546094+dfulu@users.noreply.github.com>
2025-05-14 11:43:27 +0100

    Adapt batch fix (#379)

--- pvnet/models/base_model.py
+++ pvnet/models/base_model.py
@@ -1,5 +1,5 @@
 """Base model for all PVNet submodels"""
-
+import copy
 import json
 import logging
 import os
@@ -22,6 +22,7 @@ from huggingface_hub.constants import CONFIG_NAME, PYTORCH_WEIGHTS_NAME
 from huggingface_hub.file_download import hf_hub_download
 from huggingface_hub.hf_api import HfApi
 from ocf_data_sampler.torch_datasets.sample.base import copy_batch_to_device
+from torchvision.transforms.functional import center_crop
 
 from pvnet.models.utils import (
     BatchAccumulator,
@@ -535,6 +536,51 @@ class BaseModel(pl.LightningModule, PVNetModelHubMixin):
         # save all validation results to array, so we can save these to weights n biases
         self.validation_epoch_results = []
 
+    def _adapt_batch(self, batch):
+        """Slice batches into appropriate shapes for model.
+
+        Returns a new batch dictionary with adapted data, leaving the original batch unchanged.
+        We make some specific assumptions about the original batch and the derived sliced batch:
+        - We are only limiting the future projections. I.e. we are never shrinking the batch from
+          the left hand side of the time axis, only slicing it from the right
+        - We are only shrinking the spatial crop of the satellite and NWP data
+
+        """
+        # Create a copy of the batch to avoid modifying the original
+        new_batch = {key: copy.deepcopy(value) for key, value in batch.items()}
+
+        if "gsp" in new_batch.keys():
+            # Slice off the end of the GSP data
+            gsp_len = self.forecast_len + self.history_len + 1
+            new_batch["gsp"] = new_batch["gsp"][:, :gsp_len]
+            new_batch["gsp_time_utc"] = new_batch["gsp_time_utc"][:, :gsp_len]
+
+        if self.include_sat:
+            # Slice off the end of the satellite data and spatially crop
+            # Shape: batch_size, seq_length, channel, height, width
+            new_batch["satellite_actual"] = center_crop(
+                new_batch["satellite_actual"][:, : self.sat_sequence_len],
+                output_size=self.sat_encoder.image_size_pixels,
+            )
+
+        if self.include_nwp:
+            # Slice off the end of the NWP data and spatially crop
+            for nwp_source in self.nwp_encoders_dict:
+                # shape: batch_size, seq_len, n_chans, height, width
+                new_batch["nwp"][nwp_source]["nwp"] = center_crop(
+                    new_batch["nwp"][nwp_source]["nwp"],
+                    output_size=self.nwp_encoders_dict[nwp_source].image_size_pixels,
+                )[:, : self.nwp_encoders_dict[nwp_source].sequence_length]
+
+        if self.include_sun:
+            sun_len = self.forecast_len + self.history_len + 1
+            # Slice off end of solar coords
+            for s in ["solar_azimuth", "solar_elevation"]:
+                if s in new_batch.keys():
+                    new_batch[s] = new_batch[s][:, :sun_len]
+
+        return new_batch
+
     def transfer_batch_to_device(self, batch, device, dataloader_idx):
         """Method to move custom batches to a given device"""
         return copy_batch_to_device(batch, device)
@@ -693,6 +739,10 @@ class BaseModel(pl.LightningModule, PVNetModelHubMixin):
     def training_step(self, batch, batch_idx):
         """Run training step"""
         y_hat = self(batch)
+
+        # Batch is adapted in the model forward method, but needs to be adapted here too
+        batch = self._adapt_batch(batch)
+
         y = batch[self._target_key][:, -self.forecast_len :]
 
         losses = self._calculate_common_losses(y, y_hat)
@@ -774,6 +824,8 @@ class BaseModel(pl.LightningModule, PVNetModelHubMixin):
         accum_batch_num = batch_idx // self.trainer.accumulate_grad_batches
 
         y_hat = self(batch)
+        # Batch is adapted in the model forward method, but needs to be adapted here too
+        batch = self._adapt_batch(batch)
 
         y = batch[self._target_key][:, -self.forecast_len :]
 
