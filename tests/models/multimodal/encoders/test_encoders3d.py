d86876245d37bb2494f61e8d76db0c91e13cfc35
James Fulton <41546094+dfulu@users.noreply.github.com>
2025-06-23 16:15:17 +0100

    Strip back (#377)

--- tests/models/multimodal/encoders/test_encoders3d.py
+++ tests/models/multimodal/encoders/test_encoders3d.py
@@ -1,10 +1,7 @@
 from pvnet.models.multimodal.encoders.encoders3d import (
     DefaultPVNet,
-    DefaultPVNet2,
     ResConv3DNet2,
-    EncoderUNET,
 )
-import pytest
 
 
 def _test_model_forward(batch, model_class, model_kwargs):
@@ -25,16 +22,8 @@ def test_defaultpvnet_forward(sample_satellite_batch, encoder_model_kwargs):
     _test_model_forward(sample_satellite_batch, DefaultPVNet, encoder_model_kwargs)
 
 
-def test_defaultpvnet2_forward(sample_satellite_batch, encoder_model_kwargs):
-    _test_model_forward(sample_satellite_batch, DefaultPVNet2, encoder_model_kwargs)
-
-
 def test_resconv3dnet2_forward(sample_satellite_batch, encoder_model_kwargs):
     _test_model_forward(sample_satellite_batch, ResConv3DNet2, encoder_model_kwargs)
-
-
-def test_encoderunet_forward(sample_satellite_batch, encoder_model_kwargs):
-    _test_model_forward(sample_satellite_batch, EncoderUNET, encoder_model_kwargs)
 
 
 # Test model backward on all models
@@ -42,13 +31,5 @@ def test_defaultpvnet_backward(sample_satellite_batch, encoder_model_kwargs):
     _test_model_backward(sample_satellite_batch, DefaultPVNet, encoder_model_kwargs)
 
 
-def test_defaultpvnet2_backward(sample_satellite_batch, encoder_model_kwargs):
-    _test_model_backward(sample_satellite_batch, DefaultPVNet2, encoder_model_kwargs)
-
-
 def test_resconv3dnet2_backward(sample_satellite_batch, encoder_model_kwargs):
     _test_model_backward(sample_satellite_batch, ResConv3DNet2, encoder_model_kwargs)
-
-
-def test_encoderunet_backward(sample_satellite_batch, encoder_model_kwargs):
-    _test_model_backward(sample_satellite_batch, EncoderUNET, encoder_model_kwargs)
