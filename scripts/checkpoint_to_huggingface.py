462dd20cdb51bfe9fab6dc01470a92e2e5f28c6e
Sukh-P <sukhil@openclimatefix.org>
2025-07-02 14:46:37 +0000

    Linting

--- scripts/checkpoint_to_huggingface.py
+++ scripts/checkpoint_to_huggingface.py
@@ -17,6 +17,7 @@ from pvnet.load_model import get_model_from_checkpoints
 
 app = typer.Typer(pretty_exceptions_show_locals=False)
 
+
 @app.command()
 def push_to_huggingface(
     checkpoint_dir_paths: list[str],
@@ -53,8 +54,9 @@ def push_to_huggingface(
             else:
                 wandb_ids.append(None)
 
-    model, model_config, data_config, datamodule_config = get_model_from_checkpoints(checkpoint_dir_paths,
-                                                                                     val_best)
+    model, model_config, data_config, datamodule_config = get_model_from_checkpoints(
+        checkpoint_dir_paths, val_best
+    )
 
     if not is_ensemble:
         wandb_ids = wandb_ids[0]
@@ -70,7 +72,7 @@ def push_to_huggingface(
         model_output_dir,
         config=model_config,
         data_config=data_config,
-        datamodule_config = datamodule_config,
+        datamodule_config=datamodule_config,
         wandb_repo=wandb_repo,
         wandb_ids=wandb_ids,
         push_to_hub=push_to_hub,
