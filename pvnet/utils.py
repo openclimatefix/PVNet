5463fd32c1a1d68580d5e4d25bf87cb328f8688b
James Fulton <41546094+dfulu@users.noreply.github.com>
2025-07-23 16:02:21 +0100

    Safetensors (#421)
    * Switch to safetensors for huggingface upload

--- pvnet/utils.py
+++ pvnet/utils.py
@@ -13,7 +13,7 @@ from lightning.pytorch.loggers import Logger
 from lightning.pytorch.utilities import rank_zero_only
 from omegaconf import DictConfig, OmegaConf
 
-PYTORCH_WEIGHTS_NAME = "pytorch_model.bin"
+PYTORCH_WEIGHTS_NAME = "model_weights.safetensors"
 MODEL_CONFIG_NAME = "model_config.yaml"
 DATA_CONFIG_NAME = "data_config.yaml"
 DATAMODULE_CONFIG_NAME = "datamodule_config.yaml"
